611b59608d2f0e6406cd934fd1b5ed911b5f724b
David Sarginson <david.sarginson@opencastsoftware.com>
2023-11-14 10:36:33 +0000

    adding initial workspace structure

---
+++ answers/src/main.rs
@@ -0,0 +1,5 @@
+use day01::day01_part1_answer;
+
+fn main() {
+    println!("the answer to day01 part 1 is: {}", day01_part1_answer());
+}
---
+++ day01/src/lib.rs
@@ -0,0 +1,16 @@
+use support::read_input_to_typed_groups;
+
+pub fn day01_part1_answer() -> String {
+    String::new()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test(){
+        assert_eq!(1, 1);
+    }
+}
